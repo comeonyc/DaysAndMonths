797d52ed91f8d652654e563bf5b9a1738cec512b
yc_comeon <yc_comeon@hotmail.com>
2023-02-17 22:12:05 +0800

    删除倒数第n个节点，链表

---
+++ leetcode/src/main/java/hot/RemoveNthFromEnd.java
@@ -0,0 +1,54 @@
+package hot;
+
+/**
+ * @author: yc_comeon
+ * @date: 2023/2/17
+ * @description: TODO
+ **/
+public class RemoveNthFromEnd {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 删除倒数第N个节点，关键的思路在于快慢指针，快指针先走n步
+     * 核心还是一个数学思维
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode result = head, slow = head, fast = head, pre = null;
+        while (n > 0) {
+            n--;
+            fast = fast.next;
+        }
+
+        while (null != fast) {
+            fast = fast.next;
+            pre = slow;
+            slow = slow.next;
+        }
+
+        if (null == pre) {
+            return slow.next;
+        } else {
+            pre.next = slow.next;
+        }
+        return result;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
