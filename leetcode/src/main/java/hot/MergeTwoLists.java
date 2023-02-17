ed3ccaf5a504b8bfc33811c3f12cc18734a17a08
yc_comeon <yc_comeon@hotmail.com>
2023-02-17 23:09:19 +0800

    合并两个有序链表，关键在于链表的操作

---
+++ leetcode/src/main/java/hot/MergeTwoLists.java
@@ -0,0 +1,61 @@
+package hot;
+
+/**
+ * @author: yc_comeon
+ * @date: 2023/2/17
+ * @description: TODO
+ **/
+public class MergeTwoLists {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode(-1);
+        ListNode cur = result;
+
+        while (null != list1 && null != list2) {
+            int val = -1;
+            if (list1.val < list2.val) {
+                val = list1.val;
+                list1 = list1.next;
+            } else {
+                val = list2.val;
+                list2 = list2.next;
+            }
+            cur.next = new ListNode(val);
+            cur = cur.next;
+        }
+
+        if (null != list1) {
+            cur.next = list1;
+        }
+
+        if (null != list2) {
+            cur.next = list2;
+        }
+
+        return result.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
+
